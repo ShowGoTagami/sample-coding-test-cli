b856ea3837e1077107192e1677eb3234a14af6c9
Takuto Mori Gump <mr04vv@gmail.com>
2026-01-13 16:52:12 +0900

    :wrench: chore: fix initial code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 const args = process.argv.slice(2);
 const command = args[0];
 
+if (command === undefined) {
+    console.log("No command provided");
+    process.exit(1);
+}
+
 console.log(`${command} is not implemented with args: ${args.slice(1).join(", ")}`);
 process.exit(1);
